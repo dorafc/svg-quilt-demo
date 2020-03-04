7bd74641aec9f0f0dbceab5a8a3b95568b0b92ef
Dora Fraeman Caswell <dorafraeman@gmail.com>
2020-03-04 17:33:19 -0500

    remove extra code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,3 @@ import { Quilt } from '../classes/Quilt.js';
 
 let quilt = new Quilt(newQuilt);
 quilt.renderBlocks()
-
-// insertQuiltSVG(newQuilt)
-// blocks = newQuilt.generateBlocks()
-// renderBlocks("sampleQuilt", blocks)
