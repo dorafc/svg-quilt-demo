bafc64d6452ebadcc66b2d96d8f411ff1e5f5314
Dora Fraeman Caswell <dorafraeman@gmail.com>
2020-05-20 15:30:36 -0400

    clean up comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 let blocks;      // random block types in the quilt
 
+// quilt parameters
 import { newQuilt } from '../docs/sample.js';
+
+// quilt building class
 import { Quilt } from '../classes/Quilt.js';
 
 import { EdgePattern } from "../classes/EdgePattern.js"
