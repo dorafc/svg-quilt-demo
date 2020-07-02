6fab1e2e05495f8e8824ce4d05e88bdc90010a49
Dora Fraeman Caswell <dorafraeman@gmail.com>
2020-07-02 13:21:28 -0400

    fix whitespace (oy)

--- classes/quiltRender.js
+++ classes/quiltRender.js
@@ -2,29 +2,29 @@
 Functions to render quilt SVG
 -------*/
 
-  // render blocks
-  const renderBlocks = (blocks, quiltID, debugging) => {
-    let quiltSVG = document.getElementById(quiltID)
-    blocks.forEach((block, i) => {
-      const blockGroup = block.draw(block.colors, block.startX, block.startY, block.height, block.width, block.count, debugging)
-      quiltSVG.appendChild(blockGroup)
-    })
-  }
+// render blocks
+const renderBlocks = (blocks, quiltID, debugging) => {
+  let quiltSVG = document.getElementById(quiltID)
+  blocks.forEach((block, i) => {
+    const blockGroup = block.draw(block.colors, block.startX, block.startY, block.height, block.width, block.count, debugging)
+    quiltSVG.appendChild(blockGroup)
+  })
+}
 
-  // initialize the Quilt SVG element
-  const insertQuiltSVG = (spaceName, quiltID, dimensions) => {
+// initialize the Quilt SVG element
+const insertQuiltSVG = (spaceName, quiltID, dimensions) => {
 
-    let quiltSVG, quiltSpace
+  let quiltSVG, quiltSpace
 
-    quiltSpace = document.getElementById(spaceName)
-    quiltSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+  quiltSpace = document.getElementById(spaceName)
+  quiltSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg")
 
-    quiltSVG.setAttribute("height", dimensions.quiltHeight)
-    quiltSVG.setAttribute("width", dimensions.quiltWidth)
-    quiltSVG.setAttribute("viewbox", `0 0 ${dimensions.quiltHeight} ${dimensions.quiltWidth}`)
-    quiltSVG.setAttribute("id", quiltID)
+  quiltSVG.setAttribute("height", dimensions.quiltHeight)
+  quiltSVG.setAttribute("width", dimensions.quiltWidth)
+  quiltSVG.setAttribute("viewbox", `0 0 ${dimensions.quiltHeight} ${dimensions.quiltWidth}`)
+  quiltSVG.setAttribute("id", quiltID)
 
-    quiltSpace.appendChild(quiltSVG)
-  }
+  quiltSpace.appendChild(quiltSVG)
+}
 
-  export { insertQuiltSVG, renderBlocks }
+export { insertQuiltSVG, renderBlocks }
